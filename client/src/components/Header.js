511d5749220a7fb27862d9ff4bd2ce98fbd09f46
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-09 14:04:14 -0600

    react app initialized and header created

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='home-header'>
+            <a href='/' className='header-link'>Home</a>
+        </header>
+    );
+};
+
+export default Header;
